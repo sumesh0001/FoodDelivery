02d2bc10f5edb71f83927a0fd7b8136ee7b71fa0
Sumesh sharma <sumeshsharma903@gmail.com>
2026-07-23 16:22:37 +0530

    Initial Commit

---
+++ FoodDelivery/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FoodDelivery
+//
+//  Created by Sumesh on 23/07/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FoodDelivery/FoodDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodDeliveryApp.swift
+//  FoodDelivery
+//
+//  Created by Sumesh on 23/07/26.
+//
+
+import SwiftUI
+
+@main
+struct FoodDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
